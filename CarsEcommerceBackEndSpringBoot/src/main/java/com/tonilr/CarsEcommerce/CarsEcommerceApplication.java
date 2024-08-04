67cf71b4fed1d91ded0294ab1b62924ffe8c57fc
ToniLR999 <tonilupianezroman@gmail.com>
2024-08-04 11:38:04 +0200

    Spring boot project

---
+++ CarsEcommerceBackEndSpringBoot/src/main/java/com/tonilr/CarsEcommerce/CarsEcommerceApplication.java
@@ -0,0 +1,13 @@
+package com.tonilr.CarsEcommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarsEcommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarsEcommerceApplication.class, args);
+	}
+
+}
---
+++ CarsEcommerceBackEndSpringBoot/src/test/java/com/tonilr/CarsEcommerce/CarsEcommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tonilr.CarsEcommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarsEcommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
