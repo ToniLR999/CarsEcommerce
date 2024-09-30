7ffc1741b1b5f1e0ad16104ef29d8b061905ac78
ToniLR999 <tonilupianezroman@gmail.com>
2024-09-30 23:47:35 +0200

    Services

---
+++ CarsEcommerceFrontEndAngular/src/app/services/user/user.ts
@@ -0,0 +1,10 @@
+export class User {
+    user_Id!: number;
+    username: string|undefined;
+    email: String|undefined;
+    password!: String;
+    register_date!: Date;
+    phoneNumber!: String;
+    isActive!: boolean;
+
+}
