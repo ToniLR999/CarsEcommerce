cddce144c78219b359247716c87f90ac1b79a62f
Toni <tonilupianezroman@gmail.com>
2024-10-11 23:10:13 +0200

    Table on each section of the admin panel

--- CarsEcommerceFrontEndAngular/src/app/admin-panel/admin-panel.component.ts
+++ CarsEcommerceFrontEndAngular/src/app/admin-panel/admin-panel.component.ts
@@ -6,5 +6,19 @@ import { Component } from '@angular/core';
   styleUrl: './admin-panel.component.css'
 })
 export class AdminPanelComponent {
+ // Método para mostrar la sección seleccionada
+ mostrarSeccion(seccion: string): void {
+  // Ocultar todas las secciones
+  const secciones = document.querySelectorAll('.seccion') as NodeListOf<HTMLElement>;
+  secciones.forEach((sec) => {
+    sec.style.display = 'none';
+  });
+
+  // Mostrar la sección seleccionada
+  const selectedSection = document.getElementById(seccion);
+  if (selectedSection) {
+    selectedSection.style.display = 'block';
+  }
+}
 
 }
